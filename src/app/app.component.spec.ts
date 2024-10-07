039789bfbaa0bb91383ba6f43fb02c1e86b99778
tn9th8 <tn9th8@gmail.com>
2024-10-07 17:03:10 +0700

    chore: rename to base-project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'mpk-project' title`, () => {
+  it(`should have the 'base-project' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mpk-project');
+    expect(app.title).toEqual('base-project');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, mpk-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, base-project');
   });
 });
